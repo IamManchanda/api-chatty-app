23571078927d76c16f81c7b52fa1c1e58c30364c
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-11-21 14:04:25 +0530

    Backend: Send and recieve message to diff clients

---
+++ server.js
@@ -0,0 +1,11 @@
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: ["http://localhost:8080"],
+  }
+});
+
+io.on("connection", (socket) => {
+  socket.on("send-message", (message) => {
+    socket.broadcast.emit("receive-message", message);
+  });
+});
